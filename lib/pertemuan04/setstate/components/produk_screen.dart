2388d7db3d3ddaf7d655836fb83a7aa5468f185e
Fadhil Muhammad <famuh79@gmail.com>
2022-04-06 00:11:45 +0700

    halaman 15 sekarang

--- lib/pertemuan04/setstate/components/produk_screen.dart
+++ lib/pertemuan04/setstate/components/produk_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:praktek_modul/pertemuan04/setstate/components/keranjang_screen.dart';
 
-
 class produkScreen extends StatefulWidget {
   final String username;
   produkScreen({Key? key, required this.username}) : super(key: key);
@@ -28,7 +27,7 @@ class _produkScreenState extends State<produkScreen> {
 
   @override
   void initState() {
-    // Default ctrl 
+    // Default ctrl
     sepatuCtrl.text = 0.toString();
     tasCtrl.text = 0.toString();
     celanaCtr.text = 0.toString();
@@ -46,39 +45,91 @@ class _produkScreenState extends State<produkScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(titlescreen), 
-        actions: [
-          Stack(children: [
-            IconButton(
-              icon: const Icon(Icons.shopping_cart),
-              onPressed: (){
-                //handle the press
-                Navigator.push(
-                  context, 
+      appBar: AppBar(title: Text(titlescreen), actions: [
+        Stack(children: [
+          IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {
+              //handle the press
+              Navigator.push(
+                  context,
                   MaterialPageRoute(
-                    builder: (context) => 
-                    KeranjangScreen(keranjang: keranjang)));
-              },
-              ),
+                      builder: (context) =>
+                          KeranjangScreen(keranjang: keranjang)));
+            },
+          ),
 
-              //keranjang belanja
-              Positioned(
-                child: Text(keranjang.length.toString(),
-                style: const TextStyle(fontWeight: FontWeight.bold),
-                ))
-          ])
-        ]),
-
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Padding(
+          //keranjang belanja
+          Positioned(
+              child: Text(
+            keranjang.length.toString(),
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ))
+        ])
+      ]),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
               padding: EdgeInsets.all(defaultPadding),
               child: Column(
+                children: [
+                  //header
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        child: Text(widget.username.substring(0,
+                            1)), //1 karakter. 0 sebagai karakter ke 0 lalu sebanyak 1 karakter
+                      ),
+                      SizedBox(width: defaultPadding),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Selamat Datang',
+                            style: TextStyle(fontSize: 11, color: Colors.black45),
+                          ),
+                          Text(
+                            widget.username,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              letterSpacing: 1.0),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
 
+                  const Divider(),
+                  Column(children: [
+                    //untuk kebutuhan data dinamis, level expert kunjungi
+                    // https://stackoverflow.com/questions/50766139/flutter-create-dynamic-number-of-texteditingcontrollers
+
+                    //contoh produk statik dengan textediting statik
+                    ProdukWidget(
+                      namaProduk: 'Sepatu',
+                      ctrl : sepatuCtrl,
+                      status: isSepatuAdd,
+                      press: (){
+                        print('sepatu');
+                        setState(() {
+                          isSepatuAdd = true;
+                          keranjang.add({
+                            "title": 'sepatu',
+                            "total": sepatuCtrl.text
+                          });
+                        });
+                      }
+                    ),
+                    ProdukWidget(),
+                    ProdukWidget(),
+                    ProdukWidget(),
+                  ])
+                ],
               )),
-          ),),
-          );
+        ),
+      ),
+    );
   }
 }
 
